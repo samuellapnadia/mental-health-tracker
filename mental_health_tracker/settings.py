41f503b52cc5bf76ee4d9d034e9e5be076a587b6
samuellapnadia <samuellaputrinadiapauntu@gmail.com>
2024-09-11 11:46:58 +0700

    finish tutorial 2

--- mental_health_tracker/settings.py
+++ mental_health_tracker/settings.py
@@ -122,4 +122,4 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-CSRF_TRUSTED_ORIGINS = ["http://localhost","http://127.0.0.1","https://samuella-putri-mentalhealthtracker.pbp.cs.ui.ac.id/", "https://samuella-putri-mentalhealthtracker.pbp.cs.ui.ac.id/"]
+CSRF_TRUSTED_ORIGINS = ["http://localhost","http://127.0.0.1","http://samuella-putri-mentalhealthtracker.pbp.cs.ui.ac.id/", "https://samuella-putri-mentalhealthtracker.pbp.cs.ui.ac.id/"]
