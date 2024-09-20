9a23c31c79b47ee1d6aa0a86c3ebe879bd90b948
samuellapnadia <samuellaputrinadiapauntu@gmail.com>
2024-09-20 23:39:55 +0700

    quiz no6

--- main/tests.py
+++ main/tests.py
@@ -3,6 +3,7 @@ from django.utils import timezone
 from .models import MoodEntry
 
 class MainTest(TestCase):
+    
     def test_main_url_is_exist(self):
         response = Client().get('')
         self.assertEqual(response.status_code, 200)
